7cccbba50ca29e3e016ec43c56a1cb65ba028b27
RajatDev1299 <rajatkumarbcoder.com>
2026-02-01 21:09:33 +0530

    task manger  setupcomplete

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Pokédex" }} />
-      <Stack.Screen
-        name="PokemonDetails"
-        options={{ title: "Pokémon Detail" }}
-      />
-    </Stack>
-  );
+  return <Stack />;
 }
